1ed603e2a826cb0302c0eaca60203030d94d430b
ebowen8503 <ebowen8503@gmail.com>
2017-05-09 13:54:06 -0500

    variables

---
+++ ProjectTIYFallbackend/ProjectTIYFallbackend/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTIYFallbackend
+{
+    class Class1
+    {
+        public Class1()
+        {
+            string Name = "";
+            Name = Console.ReadLine();
+            int age = 0;
+            string College;
+            College = Console.ReadLine();
+            double StudentId = 0000;
+            string Class;
+            Class = Console.ReadLine();
+
+            Console.WriteLine("Student's name");
+            Name = Console.ReadLine();
+            Console.WriteLine("Please provide your age.");
+            age = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What college do you attend?");
+            College = Console.ReadLine();
+            Console.WriteLine("Please provide your Student Id.");
+            StudentId = double.Parse(Console.ReadLine());
+            Console.WriteLine("What class are you in?");
+            Class = Console.ReadLine();
+            Console.WriteLine("The student's name is {0}. Age is {1} years old. They attend {2} University. Their student id is {3}. They are enrolled in {4}.", Name, age, College, StudentId, Class);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProjectTIYFallbackend/ProjectTIYFallbackend/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTIYFallbackend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Class1 construtorclass = new Class1();
+
+            string Name="";
+            Name = Console.ReadLine();
+            string Address;
+            Address = Console.ReadLine();
+
+            Console.WriteLine("What is your fullname?");
+                Name = Console.ReadLine();
+            Console.WriteLine("What is your complete mailing address?");
+            Address = Console.ReadLine();
+            Console.WriteLine("Your name is {0} and address is {1},", Name, Address);
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
